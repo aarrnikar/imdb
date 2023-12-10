c85e0c73e4ad0c6c7025b00f30aaf8fd297e1e33
aarrnikar <abhay.arnikar@yahoo.com>
2023-12-10 03:08:09 -0500

    add indexes and constraints for tables in create.sql

--- create.sql
+++ create.sql
@@ -4,7 +4,8 @@ CREATE TABLE name_basics (
     birthYear INT,
     deathYear INT,
     primaryProfession VARCHAR(255),
-    knownForTitles VARCHAR(255)
+    knownForTitles VARCHAR(255),
+    INDEX (primaryName)
 );
 
 CREATE TABLE title_akas (
@@ -16,7 +17,9 @@ CREATE TABLE title_akas (
     types VARCHAR(255),
     attributes VARCHAR(255),
     isOriginalTitle BOOLEAN,
-    PRIMARY KEY(titleId, ordering)
+    PRIMARY KEY(titleId, ordering),
+    FOREIGN KEY (titleId) REFERENCES title_basics(tconst),
+    INDEX (title)
 );
 
 CREATE TABLE title_basics (
@@ -28,20 +31,24 @@ CREATE TABLE title_basics (
     startYear INT,
     endYear INT,
     runtimeMinutes INT,
-    genres VARCHAR(255)
+    genres VARCHAR(255),
+    INDEX (primaryTitle)
 );
 
 CREATE TABLE title_crew (
     tconst VARCHAR(255) NOT NULL PRIMARY KEY,
     directors VARCHAR(255),
-    writers VARCHAR(255)
+    writers VARCHAR(255),
+    FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
 );
 
 CREATE TABLE title_episode (
     tconst VARCHAR(255) NOT NULL PRIMARY KEY,
     parentTconst VARCHAR(255),
     seasonNumber INT,
-    episodeNumber INT
+    episodeNumber INT,
+    FOREIGN KEY (tconst) REFERENCES title_basics(tconst),
+    FOREIGN KEY (parentTconst) REFERENCES title_basics(tconst)
 );
 
 CREATE TABLE title_principals (
@@ -51,11 +58,14 @@ CREATE TABLE title_principals (
     category VARCHAR(255),
     job VARCHAR(255),
     characters VARCHAR(255),
-    PRIMARY KEY(tconst, ordering)
+    PRIMARY KEY(tconst, ordering),
+    FOREIGN KEY (tconst) REFERENCES title_basics(tconst),
+    FOREIGN KEY (nconst) REFERENCES name_basics(nconst)
 );
 
 CREATE TABLE title_ratings (
     tconst VARCHAR(255) NOT NULL PRIMARY KEY,
     averageRating DECIMAL(3, 1),
-    numVotes INT
+    numVotes INT,
+    FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
 );
